c42193cc34975ecd7e730adbb0ac3a52682c752f
MenzurkaKBAPK <pycjlah06@gmail.com>
2025-07-02 06:19:44 +0300

    base

---
+++ src/main/java/com/someshit/SomeShitClient.java
@@ -0,0 +1,10 @@
+package com.someshit;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SomeShitClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
