626f19c7123c3d68352dfde16a13050c9aab8358
MenzurkaKBAPK <pycjlah06@gmail.com>
2025-07-02 08:14:59 +0300

    first item fix

--- src/main/java/com/someshit/item/ModItems.java
+++ src/main/java/com/someshit/item/ModItems.java
@@ -5,11 +5,13 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-    public static final Item PLACE = registerItem("place", new Item(new Item.Settings()));
+    public static final Item PLACE = registerItem("place", new Item(settings("place")));
 
 
     private static Item registerItem(String name, Item item) {
@@ -23,5 +25,9 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(PLACE);
         });
+    }
+
+    private static Item.Settings settings(String name) {
+        return new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(SomeShit.MOD_ID, name)));
     }
 }
